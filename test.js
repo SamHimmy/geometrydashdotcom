9a76d5f28d228949906f00a8b0a7585e6acbb49b
lasokar <129457995+lasokar@users.noreply.github.com>
2026-04-04 16:43:54 -0700

    add level ID

--- test.js
+++ test.js
@@ -1,19 +1,21 @@
+let levelID = null;
+
+self.addEventListener('message', event => {
+  if (event.data.levelId) {
+    levelID = event.data.levelId;
+  }
+});
+
 self.addEventListener('install', event => {
-  console.log('success installing');
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   console.log('request:', url.pathname);
-});
-
-self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-
-  if (url.pathname.includes('.txt')) {
+  if (url.pathname.includes('1.txt')) {
     event.respondWith(
-      fetch('/geometrydashdotcom/assets/2.txt')
+      fetch(`/geometrydashdotcom/levels/${levelID}.txt`)
     );
   }
 });
